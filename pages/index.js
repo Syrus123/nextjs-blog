cbe5b84f716d20def2f41924b6031e6655990bb0
Syrus123 <39728391+Syrus123@users.noreply.github.com>
2021-04-07 11:26:28 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/Posts'
+import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 export async function getStaticProps() {
